bda32b0a4ad08d55c45aee6ccbd0c1047073cdd6
Chidi Okpala <63919087+ChidiOkpala@users.noreply.github.com>
2020-08-09 17:30:01 +0100

    added the components folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Pricing from './components/pages/pricing/pricing'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>MOVIEHUB</h1>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <h1>MOVIEHUB</h1>
+    //   </header>
+    // </div>
+    <div>
+      <Pricing/>
     </div>
   );
 }
---
+++ src/components/pages/pricing/pricing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './pricing.css';
+
+function Pricing() {
+  return (
+    <div className="container">
+      <header className="pricingheader">
+        <h1>Select Your Plan</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Pricing;
