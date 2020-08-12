a1aa2ec617ea8ccb1da55d8c53a66a9ab5c5ac64
emmacodemy <eayodeleof18@gmail.com>
2020-08-12 13:28:52 +0100

    created movie detail page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import Pricing from './components/pages/pricing/pricing';
-import Signup from './components/pages/signup/Signup';
-import Discover from './components/pages/discover/discover';
-import Home from './components/pages/home/home';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import Pricing from "./components/pages/pricing/pricing";
+import Signup from "./components/pages/signup/Signup";
+import Discover from "./components/pages/discover/discover";
+import Home from "./components/pages/home/home";
+import MovieDetails from "./components/pages/movie-details/movie-details.jsx";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route exact path='/discover' component={Discover}/>
-        <Route exact path='/pricing' component={Pricing}/>
-        <Route exact path='/sign-up' component={Signup}/>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/discover" component={Discover} />
+        <Route exact path="/pricing" component={Pricing} />
+        <Route exact path="/sign-up" component={Signup} />
+        <Route exact path="/movie/:id" component={MovieDetails} />
       </Switch>
     </div>
   );
